cbf20705bb77355d78fb15f71dfedf1a553b67a7
Markus Kaihola <makeri89@hotmail.com>
2020-12-19 22:57:12 +0200

    second cypress test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon can be clicked', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
